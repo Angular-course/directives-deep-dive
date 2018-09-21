f05093f1de13fda5901b12881209ccd76c121b63
Adam Morsi <adam.morsi@yahoo.com>
2018-09-21 15:36:26 +0300

    Reactive custom attribute directive (using host listener)

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,13 +1,22 @@
 import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
-  selector: '[appBetterHighlight]'
+    selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
+    constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+    }
 
-  ngOnInit() {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
-  }
+    ngOnInit() {
+        // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    }
+
+    @HostListener('mouseenter') mouseOver(eventData: Event) {
+        this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    }
+
+    @HostListener('mouseleave') mouseleave(eventData: Event) {
+        this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    }
 }
